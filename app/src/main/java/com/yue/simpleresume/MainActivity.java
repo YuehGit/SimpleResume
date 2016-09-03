44eb22c64b235deb90fef3441a046042f6745606
Yue <yue8757@gmail.com>
2016-09-03 16:40:01 -0400

    1.6 Display multi-items in one section.

--- app/src/main/java/com/yue/simpleresume/MainActivity.java
+++ app/src/main/java/com/yue/simpleresume/MainActivity.java
@@ -2,6 +2,8 @@ package com.yue.simpleresume;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.yue.simpleresume.models.BasicInfo;
@@ -17,9 +19,9 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private BasicInfo basicInfo;
-    private Education education;
-    private Experience experience;
-    private Project project;
+    private List<Education> educations;
+    private List<Experience> experiences;
+    private List<Project> projects;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +31,8 @@ public class MainActivity extends AppCompatActivity {
         fakeData();
 
         setupBasicInfo();
-        setupEducation();
-        setupExperience();
+        setupEducations();
+        setupExperiences();
         setupProjects();
     }
 
@@ -39,32 +41,68 @@ public class MainActivity extends AppCompatActivity {
         ((TextView) findViewById(R.id.info_email)).setText(basicInfo.email);
     }
 
-    private void setupEducation() {
+    private void setupEducations() {
+        LinearLayout educationsLayout = (LinearLayout) findViewById(R.id.educations);
+        for (Education education : educations) {
+            View view = getEducationView(education);
+            educationsLayout.addView(view);
+        }
+    }
+
+    private View getEducationView(Education education) {
+        View view = getLayoutInflater().inflate(R.layout.education_item, null);
+
         String dateString = DateUtils.dateToString(education.startDate) + " ~ "
                 + DateUtils.dateToString(education.endDate);
-        ((TextView) findViewById(R.id.education_heading)).setText(education.school
+        ((TextView) view.findViewById(R.id.education_heading)).setText(education.school
                 + " - " + education.major + " - " + "(" + dateString + ")");
-        ((TextView) findViewById(R.id.education_courses))
+        ((TextView) view.findViewById(R.id.education_courses))
                 .setText(formatItems(education.courses));
+
+        return view;
     }
 
-    private void setupExperience() {
+    private void setupExperiences() {
+        LinearLayout experiencesLayout = (LinearLayout) findViewById(R.id.experiences);
+        for (Experience experience : experiences) {
+            View view = getExperienceView(experience);
+            experiencesLayout.addView(view);
+        }
+    }
+
+    private View getExperienceView(Experience experience) {
+        View view = getLayoutInflater().inflate(R.layout.experience_item, null);
+
         String dateString = DateUtils.dateToString(experience.startDate) + " ~ "
                 + DateUtils.dateToString(experience.endDate);
-        ((TextView) findViewById(R.id.experience_heading)).setText(experience.company
+        ((TextView) view.findViewById(R.id.experience_heading)).setText(experience.company
                 + " - " + experience.title + " - " + "(" + dateString + ")");
-        ((TextView) findViewById(R.id.experience_details))
+        ((TextView) view.findViewById(R.id.experience_details))
                 .setText(formatItems(experience.details));
 
+        return view;
     }
 
+
     private void setupProjects() {
+        LinearLayout projectsLayout = (LinearLayout) findViewById(R.id.projects);
+        for (Project project : projects) {
+            View view = getProjectView(project);
+            projectsLayout.addView(view);
+        }
+    }
+
+    private View getProjectView(Project project) {
+        View view = getLayoutInflater().inflate(R.layout.project_item, null);
+
         String dateString = DateUtils.dateToString(project.startDate) + " ~ "
                 + DateUtils.dateToString(project.endDate);
-        ((TextView) findViewById(R.id.projects_heading)).setText(project.name
+        ((TextView) view.findViewById(R.id.project_heading)).setText(project.name
                 + " - "  + "(" + dateString + ")");
-        ((TextView) findViewById(R.id.projects_details))
+        ((TextView) view.findViewById(R.id.project_details))
                 .setText(formatItems(project.details));
+
+        return view;
     }
 
     private void fakeData() {
@@ -72,38 +110,83 @@ public class MainActivity extends AppCompatActivity {
         basicInfo.name = "John Doe";
         basicInfo.email = "Doe.John@gmail.com";
 
-        education = new Education();
-        education.school = "UCSD";
-        education.major = "Electrical Engineering";
-        education.startDate = DateUtils.stringToDate("09/2011");
-        education.endDate = DateUtils.stringToDate("09/2014");
+        Education education1 = new Education();
+        education1.school = "UCSD";
+        education1.major = "Electrical Engineering";
+        education1.startDate = DateUtils.stringToDate("09/2011");
+        education1.endDate = DateUtils.stringToDate("09/2012");
 
-        education.courses = new ArrayList<>();
-        education.courses.add("Wireless Network");
-        education.courses.add("Communication Theory");
-        education.courses.add("Mobile Security");
-        education.courses.add("Signal Processing");
+        education1.courses = new ArrayList<>();
+        education1.courses.add("Wireless Network");
+        education1.courses.add("Communication Theory");
+        education1.courses.add("Mobile Security");
+        education1.courses.add("Signal Processing");
 
-        experience = new Experience();
-        experience.company = "Google";
-        experience.title = "Software Engineer";
-        experience.startDate = DateUtils.stringToDate("04/2015");
-        experience.endDate = DateUtils.stringToDate("09/2016");
+        Education education2 = new Education();
+        education2.school = "UCLA";
+        education2.major = "Computer Science";
+        education2.startDate = DateUtils.stringToDate("01/2013");
+        education2.endDate = DateUtils.stringToDate("09/2014");
 
-        experience.details = new ArrayList<>();
-        experience.details.add("Built something useing some tech");
-        experience.details.add("Built something useing some tech");
-        experience.details.add("Built something useing some tech");
+        education2.courses = new ArrayList<>();
+        education2.courses.add("Database");
+        education2.courses.add("Algorithm");
+        education2.courses.add("OO Design");
+        education2.courses.add("Operating System");
 
-        project = new Project();
-        project.name = "SimpleResume";
-        project.startDate = DateUtils.stringToDate("12/2014");
-        project.endDate = DateUtils.stringToDate("01/2015");
+        educations = new ArrayList<>();
+        educations.add(education1);
+        educations.add(education2);
 
-        project.details = new ArrayList<>();
-        project.details.add("Complete something using some tech");
-        project.details.add("Complete something using some tech");
-        project.details.add("Complete something using some tech");
+        Experience experience1 = new Experience();
+        experience1.company = "Google";
+        experience1.title = "Software Engineer";
+        experience1.startDate = DateUtils.stringToDate("11/2014");
+        experience1.endDate = DateUtils.stringToDate("06/2015");
+
+        experience1.details = new ArrayList<>();
+        experience1.details.add("Built something useing some tech");
+        experience1.details.add("Built something useing some tech");
+        experience1.details.add("Built something useing some tech");
+
+        Experience experience2 = new Experience();
+        experience2.company = "Facebook";
+        experience2.title = "Software Engineer";
+        experience2.startDate = DateUtils.stringToDate("08/2015");
+        experience2.endDate = DateUtils.stringToDate("09/2016");
+
+        experience2.details = new ArrayList<>();
+        experience2.details.add("Built something useing some tech");
+        experience2.details.add("Built something useing some tech");
+        experience2.details.add("Built something useing some tech");
+
+        experiences = new ArrayList<>();
+        experiences.add(experience1);
+        experiences.add(experience2);
+
+        Project project1 = new Project();
+        project1.name = "SimpleResume";
+        project1.startDate = DateUtils.stringToDate("12/2014");
+        project1.endDate = DateUtils.stringToDate("01/2015");
+
+        project1.details = new ArrayList<>();
+        project1.details.add("Complete something using some tech");
+        project1.details.add("Complete something using some tech");
+        project1.details.add("Complete something using some tech");
+
+        Project project2 = new Project();
+        project2.name = "SimpleTodo";
+        project2.startDate = DateUtils.stringToDate("06/2015");
+        project2.endDate = DateUtils.stringToDate("09/2015");
+
+        project2.details = new ArrayList<>();
+        project2.details.add("Complete something using some tech");
+        project2.details.add("Complete something using some tech");
+        project2.details.add("Complete something using some tech");
+
+        projects = new ArrayList<>();
+        projects.add(project1);
+        projects.add(project2);
     }
 
     private String formatItems(List<String> items) {
